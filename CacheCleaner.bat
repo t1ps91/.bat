d9c569f6773df41fdfb146990cfcbc7295298611
t1ps <86162677+t1ps91@users.noreply.github.com>
2022-02-01 15:16:45 +0600

    Update CacheCleaner.bat

--- CacheCleaner.bat
+++ CacheCleaner.bat
@@ -3,6 +3,7 @@ RMDIR /Q /S C:\Windows\Temp
 RMDIR /Q /S "%temp%"
 RMDIR /Q /S C:\Windows\Prefetch
 RMDIR /Q /S C:\Windows\SoftwareDistribution\Download
+RMDIR /Q /S "C:\MSOCache\All Users"
 echo Y|ipconfig/flushdns
 wsreset
 taskkill /IM WinStore.App.exe /T /F
